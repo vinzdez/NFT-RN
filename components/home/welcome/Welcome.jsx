b2084fc5c409da89d2b77fd3d1c47c8af212c077
Vicente Angcaway <vinzdez@gmail.com>
2023-04-06 06:56:35 +0800

    update state of scrrefactor screen header

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import {useState} from 'react'
+import { View,
+   Text,
+   TextInput,
+   TouchableOpacity,
+   Image,
+   FlatList } from 'react-native'
 
 import styles from './welcome.style'
+import { useRouter } from 'expo-router'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Sample</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect Job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+           <TextInput
+            style={styles.searchInput}
+          />
+        </View>
+      </View>
     </View>
   )
 }
