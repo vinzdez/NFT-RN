df35ba1687572e041da1a71f97c60af2aecfdbb6
vicente angcaway <vinzdez@gmail.com>
2023-04-06 10:11:18 +0800

    enhance Welcom UI

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -10,8 +10,11 @@ import styles from './welcome.style'
 import { useRouter } from 'expo-router'
 import { icons, SIZES } from '../../../constants'
 
+const jobTypes = ["Full-time","Part-time","Freelance","Internship","Temporary","Contractor"];
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time')
   return (
     <View>
       <View style={styles.container}>
@@ -22,8 +25,37 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
            <TextInput
             style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder = "What are you looking for?"
           />
         </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMethod="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({item}) => (
+            <TouchableOpacity 
+            style={styles.tab(activeJobType,item)}
+            onPress={() => {
+              setActiveJobType(item)
+              router.push(`/search/${item}`)
+            }}>
+              <Text style={styles.tabText(activeJobType,item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+        />
+
       </View>
     </View>
   )
